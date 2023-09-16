ae4cfac25d3f9ebd68fab78ed4196701afeb4a83
AbdullahDsouky2023 <abdullahdsouqyabdelnab@gmail.com>
2023-09-16 02:10:07 -0700

    adding new files

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+int print_char(va_list val);
+int print_string(va_list val);
+int _putchar(char c);
+#endif
